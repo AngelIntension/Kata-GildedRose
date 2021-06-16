01a1ba6df8ca7eafd66b9ff07719378d09d93bcf
Byron Burks <byron.burks@gmail.com>
2021-06-16 12:13:20 -0500

    added initial unit test for normal items

---
+++ src/GildedRose.Tests/UpdateQualityShould.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using GildedRose.Console;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GildedRose.Tests
+{
+    public class UpdateQualityShould
+    {
+        [Fact]
+        public static void DecrementQualityByOne_GivenNormalItemAndPositiveSellIn()
+        {
+            List<Item> items = new List<Item> { new Item { Name = "Normal Item", Quality = 10, SellIn = 10 } };
+            Program app = new Program(items);
+
+            app.UpdateQuality();
+
+            Assert.Equal(expected: 9, actual: app.Items.First().Quality);
+        }
+    }
+}
