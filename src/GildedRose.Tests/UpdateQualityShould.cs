60b6c6cf50cf8fe58036937726962e3f73e73012
Byron Burks <byron.burks@gmail.com>
2021-06-16 12:49:57 -0500

    added unit tests for normal items. all unit tests are passing

--- src/GildedRose.Tests/UpdateQualityShould.cs
+++ src/GildedRose.Tests/UpdateQualityShould.cs
@@ -8,14 +8,66 @@ namespace GildedRose.Tests
     public class UpdateQualityShould
     {
         [Fact]
-        public static void DecrementQualityByOne_GivenNormalItemAndPositiveSellIn()
+        public static void DecrementQualityByOne_GivenNormalItem()
         {
-            List<Item> items = new List<Item> { new Item { Name = "Normal Item", Quality = 10, SellIn = 10 } };
-            Program app = new Program(items);
+            Item normalItem = GetNormalItem();
+            int initialQuality = normalItem.Quality;
+            Program app = new Program(new List<Item> { normalItem });
 
             app.UpdateQuality();
 
-            Assert.Equal(expected: 9, actual: app.Items.First().Quality);
+            Assert.Equal(
+                expected: initialQuality - 1,
+                actual: normalItem.Quality
+                );
+        }
+
+        private static Item GetNormalItem()
+        {
+            return new Item { Name = "Normal Item", Quality = 10, SellIn = 10 };
+        }
+
+        [Fact]
+        public static void DecrementSellInByOne_GivenNormalItem()
+        {
+            Item normalItem = GetNormalItem();
+            int initialSellIn = normalItem.SellIn;
+            Program app = new Program(new List<Item> { normalItem });
+
+            app.UpdateQuality();
+
+            Assert.Equal(
+                expected: initialSellIn - 1,
+                actual: normalItem.SellIn
+                );
+        }
+
+        [Fact]
+        public static void DecrementQualityByTwo_GivenExpiredNormalItem()
+        {
+            Item normalItem = GetNormalItem();
+            normalItem.SellIn = 0;
+            int initialQuality = normalItem.Quality;
+            Program app = new Program(new List<Item> { normalItem });
+
+            app.UpdateQuality();
+
+            Assert.Equal(
+                expected: initialQuality - 2,
+                actual: normalItem.Quality
+                );
+        }
+
+        [Fact]
+        public static void ClampQualityAtZero_GivenNormalItem()
+        {
+            Item normalItem = GetNormalItem();
+            normalItem.Quality = 0;
+            Program app = new Program(new List<Item> { normalItem });
+
+            app.UpdateQuality();
+
+            Assert.False(normalItem.Quality < 0);
         }
     }
 }
