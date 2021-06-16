630f2e369a4012b4a5e11f042cedec61d20a8c36
Byron Burks <byron.burks@gmail.com>
2021-06-16 11:31:24 -0500

    Adding initial project files
    No changes made. Good build and unit tests functionality.

---
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class TestAssemblyTests
+    {
+        [Fact]
+        public static void TestTheTruth()
+        {
+            Assert.True(true);
+        }
+    }
+}
